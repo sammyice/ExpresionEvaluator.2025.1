97bc93005b6f5b19e6720585a8043bf0cf538233
Juan Carlos Zuluaga Cardona <jzuluaga@planatechnologies.com>
2025-03-13 20:36:13 -0500

    Convert infix to postfix

---
+++ Evaluator/Evaluator.Logic/FunctionEvaluator.cs
@@ -0,0 +1,94 @@
+﻿namespace Evaluator.Logic;
+
+public class FunctionEvaluator
+{
+    public static double Evalute(string infix)
+    {
+        var postfix = ToPostfix(infix);
+        Console.WriteLine(postfix);
+        return Calculate(postfix);
+    }
+
+    private static double Calculate(string postfix)
+    {
+        return 0;
+    }
+
+    private static string ToPostfix(string infix)
+    {
+        var stack = new Stack<char>();
+        var postfix = string.Empty;
+        foreach (var item in infix)
+        {
+            if (IsOperator(item))
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(item);
+                }
+                else
+                {
+                    if (item == ')')
+                    {
+                        do
+                        {
+                            postfix += stack.Pop();
+                        } while (stack.Peek() != '(');
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        if (PriorityExpression(item) > PriorityStack(stack.Peek()))
+                        {
+                            stack.Push(item);
+                        }
+                        else
+                        {
+                            postfix += stack.Pop();
+                            stack.Push(item);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                postfix += item;
+            }
+        }
+        do
+        {
+            postfix += stack.Pop();
+        } while (stack.Count > 0);
+        return postfix;
+    }
+
+    private static int PriorityStack(char item)
+    {
+        return item switch
+        {
+            '^' => 3,
+            '*' => 2,
+            '/' => 2,
+            '+' => 1,
+            '-' => 1,
+            '(' => 0,
+            _ => throw new Exception("Invalid expression."),
+        };
+    }
+
+    private static int PriorityExpression(char item)
+    {
+        return item switch
+        {
+            '^' => 4,
+            '*' => 2,
+            '/' => 2,
+            '+' => 1,
+            '-' => 1,
+            '(' => 5,
+            _ => throw new Exception("Invalid expression."),
+        };
+    }
+
+    private static bool IsOperator(char item) => "()^*/+-".IndexOf(item) >= 0;
+}
---
+++ Evaluator/Evaluator.UI.Console/Program.cs
@@ -0,0 +1,6 @@
+﻿using Evaluator.Logic;
+
+var resut1 = FunctionEvaluator.Evalute("4*(5+6-(8/2^3)-7)-1");
+var resut2 = FunctionEvaluator.Evalute("9^(1/2");
+Console.WriteLine(resut1);
+Console.WriteLine(resut2);
